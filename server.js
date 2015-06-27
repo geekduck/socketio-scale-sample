fd856b65fb3f417436f39896c7d49b93f261a999
geekduck <duck1218+github@gmail.com>
2015-06-28 03:49:31 +0900

    Socket.IO v1.3.5

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io').listen(http);
-var redis = require('socket.io/lib/stores/redis');
-var redisConf = {
-  host: '127.0.0.1',
-  port: 6379
-};
-io.set('store', new redis({
-  redisPub: redisConf,
-  redisSub: redisConf,
-  redisClient: redisConf
-}));
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var redis = require('socket.io-redis');
+io.adapter(redis({ host: 'localhost', port: 6379}));
 
-var port = 8080 + parseInt(process.env.NODE_APP_INSTANCE || 0);;
+var port = 8080 + parseInt(process.env.NODE_APP_INSTANCE || 0);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function(socket) {
+io.on('connection', function(socket) {
   socket.on('chat message', function(msg) {
-    io.sockets.emit('chat message', "port:" + port + " user:" + socket.id + " msg:" + msg);
+    io.emit('chat message', "port:" + port + " user:" + socket.id + " msg:" + msg);
   });
 });
 
